54dbbc0532b8e0ca1cc992241f017c0f679369c5
Navya Sri Koduru <kodurunavyasri17@gmail.com>
2026-06-19 16:09:23 +0530

    Initial commit

---
+++ UserRegisterAppli/src/main/java/com/example/demo/UserRegisterAppliApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class UserRegisterAppliApplication {
+
+    public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(e ->
+            System.setProperty(e.getKey(), e.getValue())
+        );
+
+        SpringApplication.run(UserRegisterAppliApplication.class, args);
+    }
+}
