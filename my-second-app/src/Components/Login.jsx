fb4e37c79df4c5505fb2722983fe23ceb9e4fa25
Navya Sri Koduru <kodurunavyasri17@gmail.com>
2026-06-20 16:13:54 +0530

    fixed login URL

--- my-second-app/src/Components/Login.jsx
+++ my-second-app/src/Components/Login.jsx
@@ -14,7 +14,7 @@ function Login(){
     }
    async function submitDetails(){
     try{
-     const res= await axios.post("http://localhost:8081/login",loginUser)
+     const res = await axios.post("https://studentmanagement-6224.onrender.com/login", loginUser)
      console.log(res.data)
      alert("User Login SuccessFull")
      localStorage.setItem("user",JSON.stringify(res.data))
